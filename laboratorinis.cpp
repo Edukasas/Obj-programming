e3ef3c9da5a237b41f82965bf8cdb7b26949f739
Edukasas <137073027+Edukasas@users.noreply.github.com>
2024-02-09 11:17:49 +0200

    v0.1

--- laboratorinis.cpp
+++ laboratorinis.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct data {
@@ -11,24 +12,34 @@ struct data {
     double homeWorkSum = 0, finalMark;
 };
 
-double count(double homeWorkSum, int EgzamRez);
-void input(auto &student, int &totalNumberOfStudents);
-void output(auto &student, int totalNumberOfStudents);
+double countMedian(vector<int> homeWorkRez, int egzamRez);
+double countAverage(double homeWorkSum, int EgzamRez);
+void input(auto &student, int &totalNumberOfStudents, bool &option);
+void output(auto &student, int totalNumberOfStudents, bool option);
 int main() {
     vector<data> student;
     int totalNumberOfStudents;
+    bool option;
     cout<<"Moksleiviu skaicius"<<endl;
     cin>>totalNumberOfStudents;
-    input(student, totalNumberOfStudents);
-    output(student, totalNumberOfStudents);
+    input(student, totalNumberOfStudents, option);
+    output(student, totalNumberOfStudents, option);
     return 0;
 }
 
-double count(double homeWorkSum, int egzamRez) {
+double countAverage(double homeWorkSum, int egzamRez) {
     return 0.4 * homeWorkSum + egzamRez * 0.6;
 }
-
-void input(auto &student, int &totalNumberOfStudents) {
+double countMedian(vector<int> homeWorkRez, int egzamRez) {
+    sort(homeWorkRez.begin(), homeWorkRez.end());
+    int n = homeWorkRez.size();
+    if (n % 2 == 0) {
+        return 0.4 * (homeWorkRez[n / 2 - 1] + homeWorkRez[n / 2]) / 2.0  + egzamRez * 0.6;
+    } else {
+        return 0.4 * homeWorkRez[n / 2] + egzamRez * 0.6;
+    }
+}
+void input(auto &student, int &totalNumberOfStudents, bool &option) {
         for(int j = 0; j < totalNumberOfStudents; j++) {
         data newStudent;
         cout << "Studento vardas" << endl;
@@ -47,16 +58,30 @@ void input(auto &student, int &totalNumberOfStudents) {
         newStudent.homeWorkSum /= newStudent.n;
         cout << "Egzamino rezultatas" << endl;
         cin >> newStudent.egzamRez;
-        newStudent.finalMark = count(newStudent.homeWorkSum, newStudent.egzamRez);
+        cout<< "jei norit pamatyti mediana, spauskit 0, priesingu atveju pamatysit vidurki"<<endl;
+        cin >> option;
+       if(option) newStudent.finalMark = countAverage(newStudent.homeWorkSum, newStudent.egzamRez);
+         else newStudent.finalMark = countMedian(newStudent.homeWorkRez, newStudent.egzamRez);
         student.push_back(newStudent);
     }
 }
 
-void output(auto &student, int totalNumberOfStudents) {
+void output(auto &student, int totalNumberOfStudents, bool option) {
+    if(option){
     cout<<setw(15)<<left<<"Pavardė"<<setw(15)<<left<<"Vardas"<<setw(15)<<left<<"Galutinis (Vid.)"<<endl;
     cout<<"----------------------------------------------"<<endl;
     for(int i = 0; i < totalNumberOfStudents; i++){
         cout<<setw(15)<<left<<student[i].surname<<setw(15)<<left<<student[i].name<<setw(15)<<left<<student[i].finalMark<<endl;
     }
    cout<<"----";
+    }
+    else{
+            cout<<setw(15)<<left<<"Pavardė"<<setw(15)<<left<<"Vardas"<<setw(15)<<left<<"Galutinis (Med.)"<<endl;
+    cout<<"----------------------------------------------"<<endl;
+    for(int i = 0; i < totalNumberOfStudents; i++){
+        cout<<setw(15)<<left<<student[i].surname<<setw(15)<<left<<student[i].name<<setw(15)<<left<<student[i].finalMark<<endl;
+    }
+   cout<<"----";
+    }
+
 }
