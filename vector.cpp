b5de42f861b2723e275ee0713024f20c2e404ad0
Edukasas <137073027+Edukasas@users.noreply.github.com>
2024-02-17 21:31:13 +0200

    veikia be defektu

--- vector.cpp
+++ vector.cpp
@@ -7,11 +7,12 @@
 #include <ctime>
 using namespace std;
 
-struct data {
+struct data
+{
     string name;
     string surname;
     int egzamRez;
-    vector<int> homeWorkRez; 
+    vector<int> homeWorkRez;
     double homeWorkSum = 0;
     double finalMarkAverage;
     double finalMarkMedian;
@@ -28,14 +29,15 @@ int options();
 int getIntegerInput();
 int studentNumber();
 string nameValidation();
-int main() {
+int main()
+{
     srand(time(nullptr));
 
     vector<data> student;
-    
+
     input(student);
-    
-    cout<< "Spauspkit 1, kad skaiciuotu galutini bala pagal vidurki, spauskite 0, kad skaiciuotu galutini bala pagal mediana"<<endl;
+
+    cout << "Spauspkit 1, kad skaiciuotu galutini bala pagal vidurki, spauskite 0, kad skaiciuotu galutini bala pagal mediana" << endl;
     bool option = getBinaryInput();
     output(student, option);
     return 0;
@@ -48,140 +50,174 @@ string generateRandomSurname()
 {
     return surnames[rand() % surnames.size()];
 }
-void data::randomRez(){
+void data::randomRez()
+{
     int randomNumberOfHomeWork = rand() % 100 + 1;
     int help = rand() % 10 + 1;
     egzamRez = help;
-    for(int i = 0; i < randomNumberOfHomeWork; i++){
+    for (int i = 0; i < randomNumberOfHomeWork; i++)
+    {
         help = rand() % 10 + 1;
         homeWorkRez.push_back(help);
         homeWorkSum += help;
     }
     homeWorkSum = homeWorkSum / randomNumberOfHomeWork;
 }
-double data::countAverage() {
+double data::countAverage()
+{
     return 0.4 * homeWorkSum + egzamRez * 0.6;
 }
-double data::countMedian() {
+double data::countMedian()
+{
     sort(homeWorkRez.begin(), homeWorkRez.end());
     int n = homeWorkRez.size();
-    if (n > 0){
-        if (n % 2 == 0) {
-            return 0.4 * (homeWorkRez[n / 2 - 1] + homeWorkRez[n / 2]) / 2.0  + egzamRez * 0.6;
-        } else {
+    if (n > 0)
+    {
+        if (n % 2 == 0)
+        {
+            return 0.4 * (homeWorkRez[n / 2 - 1] + homeWorkRez[n / 2]) / 2.0 + egzamRez * 0.6;
+        }
+        else
+        {
             return 0.4 * homeWorkRez[n / 2] + egzamRez * 0.6;
         }
     }
-    else return egzamRez * 0.6;
-
+    else
+        return egzamRez * 0.6;
 }
-void input(auto &student) {
+void input(auto &student)
+{
     data newStudent;
     int workMethods = 0, studentCount;
     string name;
-    
-    while (workMethods != 4) {
+
+    while (workMethods != 4)
+    {
         cout << "Pasirinkimai 1 - random vardas/pavarde ir pazymiai, 2 - random pazymiai, 3 - ranka ivesti duomenys, 4 - atvaizduoti" << endl;
         workMethods = options();
         string surnameLetter = "Studento pavarde (max 20)", nameLetter = "Studento vardas (max 20) (Iveskite 'b' norint pabaigti)";
-        switch (workMethods) {
-            case 1:
-                cout << "Iveskite studentu skaiciu" << endl;
-                studentCount = studentNumber();
-                for (int i = 0; i < studentCount; i++) {
-                    newStudent.name = generateRandomName();
-                    newStudent.surname = generateRandomSurname();
-                    newStudent.randomRez();
-                    student.push_back(newStudent); // Push each student into the vector
-                }
-                break;
-            case 2:
-                cin.ignore();
-                while (true) {
-                    cout<<"Studento vardas (max 20) (Iveskite 'b' norint pabaigti)"<<endl;
-                    newStudent.name = nameValidation();
-                    if (newStudent.name == "b") break;
-                    cout<< "Studento pavarde (max 20)"<<endl;
-                    newStudent.surname = nameValidation();
-                    newStudent.randomRez();
-                    student.push_back(newStudent); // Push each student into the vector
-                }
-                break;
-            case 3:
+        switch (workMethods)
+        {
+        case 1:
+            cout << "Iveskite studentu skaiciu" << endl;
+            studentCount = studentNumber();
+            for (int i = 0; i < studentCount; i++)
+            {
+                newStudent.name = generateRandomName();
+                newStudent.surname = generateRandomSurname();
+                newStudent.randomRez();
+                student.push_back(newStudent); // Push each student into the vector
+            }
+            break;
+        case 2:
+            cin.ignore();
+            while (true)
+            {
+                cout << "Studento vardas (max 20) (Iveskite 'b' norint pabaigti)" << endl;
+                newStudent.name = nameValidation();
+                if (newStudent.name == "b")
+                    break;
+                cout << "Studento pavarde (max 20)" << endl;
+                newStudent.surname = nameValidation();
+                newStudent.randomRez();
+                student.push_back(newStudent); // Push each student into the vector
+            }
+            break;
+        case 3:
 
-                while(true){
+            while (true)
+            {
                 cin.ignore();
 
-                                       cout<<"Studento vardas (max 20) (Iveskite 'b' norint pabaigti)"<<endl;
-                    newStudent.name = nameValidation();
-                    if (newStudent.name == "b") break;
-                    cout<< "Studento pavarde (max 20)"<<endl;
-                    newStudent.surname = nameValidation();
-                    while(true){
-                        cout<<"Namu darbu ivertinimas (Iveskite '-1' norint pabaigti)"<<endl;
-                        int help = getIntegerInput();
-                            if (help == -1) {
-                                break;
-                            }
-                        newStudent.homeWorkRez.push_back(help);
-                        newStudent.homeWorkSum += help;
+                cout << "Studento vardas (max 20) (Iveskite 'b' norint pabaigti)" << endl;
+                newStudent.name = nameValidation();
+                if (newStudent.name == "b")
+                    break;
+                cout << "Studento pavarde (max 20)" << endl;
+                newStudent.surname = nameValidation();
+                while (true)
+                {
+                    cout << "Namu darbu ivertinimas (Iveskite '-1' norint pabaigti)" << endl;
+                    int help = getIntegerInput();
+                    if (help == -1)
+                    {
+                        break;
                     }
-                if(newStudent.homeWorkRez.size() != 0)newStudent.homeWorkSum /= newStudent.homeWorkRez.size();
-                else newStudent.homeWorkSum = 0;
-                cout<<"Egzamino ivertinimas"<<endl;
+                    newStudent.homeWorkRez.push_back(help);
+                    newStudent.homeWorkSum += help;
+                }
+                if (newStudent.homeWorkRez.size() != 0)
+                    newStudent.homeWorkSum /= newStudent.homeWorkRez.size();
+                else
+                    newStudent.homeWorkSum = 0;
+                cout << "Egzamino ivertinimas" << endl;
                 newStudent.egzamRez = getIntegerInput();
                 student.push_back(newStudent);
-                }
-                break;
+            }
+            break;
         }
     }
 
     // Calculating final marks outside the loop
-    for (auto &s : student) {
+    for (auto &s : student)
+    {
         s.finalMarkAverage = s.countAverage();
         s.finalMarkMedian = s.countMedian();
     }
 }
 
-void output(auto &student, bool option) {
-    if(option){
-    cout<<setw(15)<<left<<"Pavardė"<<setw(15)<<left<<"Vardas"<<setw(15)<<left<<"Galutinis (Vid.)"<<endl;
-    cout<<"----------------------------------------------"<<endl;
-    for(int i = 0; i < student.size(); i++){
-        cout<<setw(15)<<left<<student[i].surname<<setw(15)<<left<<student[i].name<<setw(15)<<left<<fixed<<setprecision(2)<<student[i].finalMarkAverage<<endl;
+void output(auto &student, bool option)
+{
+    if (option)
+    {
+        cout << setw(15) << left << "Pavardė" << setw(15) << left << "Vardas" << setw(15) << left << "Galutinis (Vid.)" << endl;
+        cout << "----------------------------------------------" << endl;
+        for (int i = 0; i < student.size(); i++)
+        {
+            cout << setw(15) << left << student[i].surname << setw(15) << left << student[i].name << setw(15) << left << fixed << setprecision(2) << student[i].finalMarkAverage << endl;
+        }
+        cout << "----";
     }
-   cout<<"----";
+    else
+    {
+        cout << setw(15) << left << "Pavardė" << setw(15) << left << "Vardas" << setw(15) << left << "Galutinis (Med.)" << endl;
+        cout << "----------------------------------------------" << endl;
+        for (int i = 0; i < student.size(); i++)
+        {
+            cout << setw(15) << left << student[i].surname << setw(15) << left << student[i].name << setw(15) << left << fixed << setprecision(2) << student[i].finalMarkMedian << endl;
+        }
+        cout << "----";
     }
-    else{
-            cout<<setw(15)<<left<<"Pavardė"<<setw(15)<<left<<"Vardas"<<setw(15)<<left<<"Galutinis (Med.)"<<endl;
-    cout<<"----------------------------------------------"<<endl;
-    for(int i = 0; i < student.size(); i++){
-        cout<<setw(15)<<left<<student[i].surname<<setw(15)<<left<<student[i].name<<setw(15)<<left<<fixed<<setprecision(2)<<student[i].finalMarkMedian<<endl;
-    }
-   cout<<"----";
-    }
-
 }
-int options() {
+int options()
+{
     int number;
     string input;
-    while (true) {
+    while (true)
+    {
         cin >> input;
         bool isValid = true;
-        for (char c : input) {
-            if (!isdigit(c)) {
+        for (char c : input)
+        {
+            if (!isdigit(c))
+            {
                 isValid = false;
                 break;
             }
         }
-        if (isValid) {
+        if (isValid)
+        {
             stringstream ss(input);
-            if (ss >> number && ss.eof()) {
+            if (ss >> number && ss.eof())
+            {
                 // Input is a valid integer
-                if (number > 0 && number < 5) {
+                if (number > 0 && number < 5)
+                {
                     // Number is greater than 0
                     break;
-                } else {
+                }
+                else
+                {
                     cout << "Pasirinkimai 1 - random vardas/pavarde ir pazymiai, 2 - random pazymiai, 3 - ranka ivesti duomenys, 4 - atvaizduoti" << endl;
                 }
             }
@@ -192,26 +228,35 @@ int options() {
     }
     return number;
 }
-int studentNumber() {
+int studentNumber()
+{
     int number;
     string input;
-    while (true) {
+    while (true)
+    {
         cin >> input;
         bool isValid = true;
-        for (char c : input) {
-            if (!isdigit(c)) {
+        for (char c : input)
+        {
+            if (!isdigit(c))
+            {
                 isValid = false;
                 break;
             }
         }
-        if (isValid) {
+        if (isValid)
+        {
             stringstream ss(input);
-            if (ss >> number && ss.eof()) {
+            if (ss >> number && ss.eof())
+            {
                 // Input is a valid integer
-                if (number > 0 && number < 1000) {
+                if (number > 0 && number < 1000)
+                {
                     // Number is greater than 0
                     break;
-                } else {
+                }
+                else
+                {
                     cout << "Studentu skaicius turi buti daugiau nei 0, bet maziau uz 1000" << endl;
                 }
             }
@@ -222,29 +267,39 @@ int studentNumber() {
     }
     return number;
 }
-int getIntegerInput() {
+int getIntegerInput()
+{
     int number;
     string input;
-    while (true) {
+    while (true)
+    {
         cin >> input;
-         if (input == "-1") {
+        if (input == "-1")
+        {
             return -1;
         }
         bool isValid = true;
-        for (char c : input) {
-            if (!isdigit(c)) {
+        for (char c : input)
+        {
+            if (!isdigit(c))
+            {
                 isValid = false;
                 break;
             }
         }
-        if (isValid) {
+        if (isValid)
+        {
             stringstream ss(input);
-            if (ss >> number && ss.eof()) {
+            if (ss >> number && ss.eof())
+            {
                 // Input is a valid integer
-                if (number > 0 && number <= 10) {
+                if (number > 0 && number <= 10)
+                {
                     // Number is greater than 0
                     break;
-                } else {
+                }
+                else
+                {
                     cout << "Skaicius turi buti didesnis uz 0, bet nedidesnis 10" << endl;
                 }
             }
@@ -255,21 +310,29 @@ int getIntegerInput() {
     }
     return number;
 }
-int getBinaryInput() {
+int getBinaryInput()
+{
     int number;
     string input;
-    while (true) {
+    while (true)
+    {
         cin >> input;
         stringstream ss(input);
-        if (ss >> number && ss.eof()) {
+        if (ss >> number && ss.eof())
+        {
             // Input is a valid integer
-            if (number == 0 || number == 1) {
+            if (number == 0 || number == 1)
+            {
                 // Number is either 0 or 1
                 break;
-            } else {
+            }
+            else
+            {
                 cout << "Iveskiti arba 0, arba 1" << endl;
             }
-        } else {
+        }
+        else
+        {
             cout << "Iveskiti arba 0, arba 1" << endl;
         }
         cin.clear();
@@ -277,24 +340,23 @@ int getBinaryInput() {
     }
     return number;
 }
-string nameValidation(){
+string nameValidation()
+{
     string name;
-            if (cin.fail()) {
-                cin.clear();
+    if (cin.fail())
+    {
+        cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         return nameValidation();
     }
-        getline(cin, name);
+    getline(cin, name);
 
-        while (!all_of(name.begin(), name.end(), [](unsigned char c)
-                { return isalpha(c) || (c == ' ' && !isspace(*(next(&c))) && !isspace(*(prev(&c)))); }) 
-        || name.length() > 20
-        || name.length() == 0
-        || (name.length() > 0 && name.front() == ' ')
-        || (name.length() > 0 && name.back() == ' '))
-         {  
-            cout << "Netinkama ivestis" <<endl;
-            getline(cin, name);
-        };
-        return name;
+    while (!all_of(name.begin(), name.end(), [](unsigned char c)
+                   { return isalpha(c) || (c == ' ' && !isspace(*(next(&c))) && !isspace(*(prev(&c)))); }) ||
+           name.length() > 20 || name.length() == 0 || (name.length() > 0 && name.front() == ' ') || (name.length() > 0 && name.back() == ' '))
+    {
+        cout << "Netinkama ivestis" << endl;
+        getline(cin, name);
+    };
+    return name;
 }
