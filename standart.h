776722cf8f0426447404681b19ddef138d8e415a
Edukasas <137073027+Edukasas@users.noreply.github.com>
2024-02-23 14:24:47 +0200

    refactor: using std::and lbs

---
+++ standart.h
@@ -0,0 +1,38 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+
+using std::endl;
+using std::cin;
+using std::cout;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::stringstream;
+using std::istringstream;
+using std::setw;
+using std::left;
+using std::setprecision;
+using std::numeric_limits;
+using std::streamsize;
+using std::sort;
+using std::getline;
+using std::cerr;
+using std::all_of;
+using std::isalpha;
+using std::isspace;
+using std::next;
+using std::prev;
+
+#endif /* COMMON_HPP */
