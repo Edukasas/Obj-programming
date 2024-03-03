1ef3e561d0d938e964ac116d19169933942df0a4
Edukasas <137073027+Edukasas@users.noreply.github.com>
2024-03-03 20:41:29 +0200

    feat: can choose to generate files

---
+++ topSection.cpp
@@ -0,0 +1,35 @@
+#include "functions.h"
+
+void topSection(vector<data> &student, int nameLenght, string filename)
+{
+    while (true)
+    {
+        try
+        {
+            cout << "0 - Ivestis per ekrana, 1 - Ivestis per faila, 2 - Generuoti faila" << endl;
+            string a_str;
+            cin >> a_str;
+            if (!isNumeric(a_str))
+            {
+                throw invalid_argument("Invalid input for 'n'");
+            }
+            int a = stoi(a_str);
+            if (a == 0){
+            input(student);
+            break;
+            }
+            else if (a == 1){
+            readStudentsFromFile(filename, student);
+            break;
+            }
+            // else if (a == 2)
+            //     generateFile(nameLenght);
+            else
+                throw invalid_argument("Netinkama ivestis");
+        }
+        catch (const invalid_argument &ex)
+        {
+            cerr << "Error: " << ex.what() << endl;
+        }
+    }
+}
