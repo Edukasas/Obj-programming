993b2248963ed2c39d742c5dc435657508d1cd31
Edgaras <edasedgaras.dir@gmail.com>
2024-03-19 15:53:05 +0200

    refactor: new folder

---
+++ vector/standart.h
@@ -0,0 +1,48 @@
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+#include <stdexcept>
+#include <chrono>
+
+const int STUDENT_SIZE = 1000000;
+
+using std::endl;
+using std::cin;
+using std::cout;
+using std::vector;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::stringstream;
+using std::istringstream;
+using std::setw;
+using std::left;
+using std::setprecision;
+using std::numeric_limits;
+using std::streamsize;
+using std::sort;
+using std::getline;
+using std::cerr;
+using std::all_of;
+using std::isalpha;
+using std::isspace;
+using std::next;
+using std::prev;
+using std::to_string;
+using std::isdigit;
+using std::stoi;
+using std::runtime_error;
+using std::invalid_argument;
+using std::exception;
+using std::bad_alloc;
+#endif
